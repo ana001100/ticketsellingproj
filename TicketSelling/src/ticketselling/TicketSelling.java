cb6db4348ab9d5cfcd4af6200f03cc0f3d88868f
ana001100 <ana.francine0527@gmail.com>
2024-11-29 16:47:11 +0800

    Add files via upload

---
+++ TicketSelling/src/ticketselling/TicketSelling.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ticketselling;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author anafr
+ */
+public class TicketSelling extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here 
+    }
+    
+}
---
+++ TicketSelling/src/ticketselling/getset.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ticketselling;
+
+/**
+ *
+ * @author anafr
+ */
+public class getset {
+
+    /**
+     * @return the seat1
+     */
+    public String getSeat1() {
+        return seat1;
+    }
+
+    /**
+     * @param seat1 the seat1 to set
+     */
+    public void setSeat1(String seat1) {
+        this.seat1 = seat1;
+    }
+
+    /**
+     * @return the seat2
+     */
+    public String getSeat2() {
+        return seat2;
+    }
+
+    /**
+     * @param seat2 the seat2 to set
+     */
+    public void setSeat2(String seat2) {
+        this.seat2 = seat2;
+    }
+    private String seat1;
+    private String seat2;
+}
